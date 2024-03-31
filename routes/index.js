ab10d7e28cd31be127ad7a7d1fd12f33f4b49936
shawn0rion <shawnorion07@gmail.com>
2024-03-31 16:30:33 -0500

    update database funcions

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,32 @@ var express = require("express");
 var router = express.Router();
 const Project = require("../models/project");
 
-router.post("/api/project", function (req, res, next) {
-  const { name, date, time } = req.body;
+router.post("/api/project", async function (req, res, next) {
+  try {
+    const { name, date, time } = req.body;
 
-  // Create project
-  const project = new Project({ name, date, time });
+    // Create project
+    const project = new Project({ name, date, time });
 
-  // Save project to database
-  project.save(function (err) {
-    if (err) {
-      // Return error message if there's an error
-      return res.status(500).json({ error: "Failed to create project" });
-    }
+    // Save project to database
+    await project.save();
 
     // Return project object if successful
     return res.status(200).json(project);
-  });
+  } catch (err) {
+    // Return error message if there's an error
+    return res.status(500).json({ error: "Failed to create project" });
+  }
 });
 
 // router.get /api/projects
-router.get("/api/projects", function (req, res, next) {
-  Project.find({}, function (err, projects) {
-    if (err) {
-      return res.status(500).json({ error: "Failed to get projects" });
-    }
-
+router.get("/api/projects", async function (req, res, next) {
+  try {
+    const projects = await Project.find({});
     return res.status(200).json(projects);
-  });
+  } catch (err) {
+    return res.status(500).json({ error: "Failed to get projects" });
+  }
 });
 
 /* GET home page. */
